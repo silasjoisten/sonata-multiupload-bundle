ed50b83faf10171e03668c0978fdccfd102f7e8d
Silas Joisten <silasjoisten@gmail.com>
2019-01-19 14:15:33 +0100

    fixed deprecation message of treebuilder (#51)
    * fixed deprecation message of treebuilder
    
    * keep compability with symfony/config < 4.2

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -12,8 +12,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('sonata_multi_upload');
+        $treeBuilder = new TreeBuilder('sonata_multi_upload');
+
+        // Keep compatibility with symfony/config < 4.2
+        if (!method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->root('sonata_admin');
+        } else {
+            $rootNode = $treeBuilder->getRootNode();
+        }
 
         $rootNode
             ->children()
