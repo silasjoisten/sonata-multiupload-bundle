5bf73cd4d7c640f5827dbd2377d62b85a2087b5b
Silas Joisten <silasjoisten@gmail.com>
2022-05-14 12:43:28 +0200

    Fixes: Controller, Admin Extension

--- src/Admin/MultiUploadAdminExtension.php
+++ src/Admin/MultiUploadAdminExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SilasJoisten\Sonata\MultiUploadBundle\Admin;
 
+use SilasJoisten\Sonata\MultiUploadBundle\Controller\MultiUploadController;
 use Sonata\AdminBundle\Admin\AbstractAdminExtension;
 use Sonata\AdminBundle\Admin\AdminInterface;
 use Sonata\AdminBundle\Route\RouteCollectionInterface;
@@ -12,6 +13,8 @@ final class MultiUploadAdminExtension extends AbstractAdminExtension
 {
     public function configureRoutes(AdminInterface $admin, RouteCollectionInterface $collection): void
     {
-        $collection->add('multi_upload', 'multi-upload');
+        $collection->add('multi_upload', 'multi-upload', [
+            '_controller' => sprintf('%s::multiUpload', MultiUploadController::class),
+        ]);
     }
 }
--- src/Controller/MediaAdminController.php
+++ src/Controller/MediaAdminController.php
@@ -14,17 +14,19 @@ use Symfony\Component\HttpFoundation\Response;
  */
 final class MediaAdminController extends CRUDController
 {
+    public function __construct(
+        private Pool $pool,
+    ) {
+    }
+
     public function createAction(Request $request): Response
     {
         $this->admin->checkAccess('create');
 
         if (null === $request->get('provider') && $request->isMethod('get')) {
-            $pool = $this->container->get('sonata.media.pool');
-            \assert($pool instanceof Pool);
-
-            $this->renderWithExtraParams('@SonataMultiUpload/select_provider.html.twig', [
-                'providers' => $pool->getProvidersByContext(
-                    $request->get('context', $pool->getDefaultContext())
+            return $this->renderWithExtraParams('@SonataMultiUpload/select_provider.html.twig', [
+                'providers' => $this->pool->getProvidersByContext(
+                    $request->get('context', $this->pool->getDefaultContext())
                 ),
                 'action' => 'create',
             ]);
